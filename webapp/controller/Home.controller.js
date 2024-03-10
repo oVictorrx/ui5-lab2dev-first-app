2f841b6bda9934bce66ce585dda55a7375f1d129
oVictorrx <ovictorhue@gmail.com>
2024-03-10 12:30:50 -0300

    add: mensagem ao clicar no produto

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,15 +1,25 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/m/MessageBox"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, MessageToast, MessageBox) {
+    "use strict";
 
-        return Controller.extend("com.lab2dev.firstapp.controller.Home", {
-            onInit: function () {
+    return Controller.extend("com.lab2dev.firstapp.controller.Home", {
+      onInit: function () {},
 
-            }
+      onPress: function (oEvent) {
+        const item = oEvent.getSource();
+
+        const itemTitle = item.getTitle();
+
+        const message = `O item: "${itemTitle}" foi clicado!`;
+
+        MessageBox.show(message, {
+          title: "Informação do item",
         });
+      },
     });
+  }
+);
